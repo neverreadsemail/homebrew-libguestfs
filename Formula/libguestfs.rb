0a28ef4e7924371be47e28ef6bf6165c74ed0a43
Allison Andrews <allie@slackers.net>
2022-10-11 19:49:41 -0700

    changes to build on modern macOS/aarch64

--- Formula/libguestfs.rb
+++ Formula/libguestfs.rb
@@ -6,8 +6,8 @@ class OsxfuseRequirement < Requirement
   satisfy(build_env: false) { self.class.binary_osxfuse_installed? }
 
   def self.binary_osxfuse_installed?
-    File.exist?("/usr/local/include/osxfuse/fuse/fuse.h") &&
-      !File.symlink?("/usr/local/include/osxfuse/fuse")
+    File.exist?("/usr/local/include/fuse/fuse.h") &&
+      !File.symlink?("/usr/local/include/fuse")
   end
 
   env do
@@ -25,8 +25,14 @@ end
 class Libguestfs < Formula
   desc "Tools for accessing and modifying virtual machine disk images"
   homepage "https://libguestfs.org/"
-  url "https://download.libguestfs.org/1.44-stable/libguestfs-1.44.1.tar.gz"
-  sha256 "72b7dcdd32da1c17c932cf5a0a70b3bd68bc93e94828ad66a539f2e616adb025"
+  # 1.48.4 is the latest stable release, but there have been lots of macOS
+  # related patches since then. build from development until I find the minimum
+  # set of required patches.
+  # url "https://libguestfs.org/download/1.48-stable/libguestfs-1.48.4.tar.gz"
+  # sha256 "9dc22b6c5a45f19c2cba911a37b3a8d86f62744521b10eb53c3d3907e5080312"
+  url "https://libguestfs.org/download/1.49-development/libguestfs-1.49.5.tar.gz"
+  sha256 "7923af8a5e2aa44268a5fed3cfb0634884e6562c88f46af65f066ce6a74547c4"
+  head "https://github.com/libguestfs/libguestfs.git"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -68,35 +74,30 @@ class Libguestfs < Formula
 
   # Since we can't build an appliance, the recommended way is to download a fixed one.
   resource "fixed_appliance" do
-    url "https://download.libguestfs.org/binaries/appliance/appliance-1.40.1.tar.xz"
-    sha256 "1aaf0bef18514b8e9ebd0c6130ed5188b6f6a7052e4891d5f3620078f48563e6"
+    url "file:///tmp/appliance-1.44.0.tar.xz"
+    sha256 "622b222c18882455e55745531b1d06e4663b25558e5f1123a375ab6da346042c"
   end
 
-  # patch do
-  #   # program_name and open_memstream.c
-  #   url "https://gist.githubusercontent.com/Amar1729/541e66dff14fec0100931b64f78b8f38/raw/27a13176be00ab7e3a13f3eec536b60709c30043/libguestfs-gnulib.patch"
-  #   sha256 "621269d78db5cf15e2961189d7714cfb3b6687bdd4d0d4be6b94b4d866e43c7e"
+  # stable do
+  #   patch do
+  #     url "https://github.com/libguestfs/libguestfs/commit/6c0e5d7f8f2a8eaddadbc34c08f8a1ed095626b0.diff"
+  #     sha256 "e1d692c864646db3f0bc516f2bf595d878ee83ec4cf71d18c898e192fec32f04"
+  #   end
+  #   patch do
+  #     url "https://github.com/libguestfs/libguestfs/commit/ef947a9d3ba8e2c55a05a25e8b8e55d1f3094f72.diff"
+  #     sha256 "14cae217576bc2fb399cfafb11cf8c545998a29dfe913dcde1cb5affd0e1bd10"
+  #   end
+  #   patch do
+  #     url "https://github.com/libguestfs/libguestfs/commit/8d5063774111800b907742cdab19a4fde530b325.diff"
+  #     sha256 "7dc6bc945261ab7179f1f266319c9a5ebec46ce4a9879bce3513bc1da5066380"
+  #   end
   # end
-
-  # The two required gnulib patches have been reported to gnulib mailing list, but with little effect so far.
-  # patch do
-  #   # Add an implementation of open_memstream for BSD/Mac.
-  #   # Using Eric Blake's proposal originally published here: https://lists.gnu.org/archive/html/bug-gnulib/2010-04/msg00379.html
-  #   # and mentioned again here: http://lists.gnu.org/archive/html/bug-gnulib/2015-02/msg00083.html
-  #   url "https://gist.githubusercontent.com/shulima/93138eb342fe94273edd/raw/c75eac3a7f536dca526f52cd8cb5c0d6ce8beecc/gnulib-open_memstream.patch"
-  #   sha256 "d62f539def7300e4155bf2447b3c22049938a279957a4a97964d2d04440b58ce"
-  # end
-  # patch do
-  #   # Add a program_name equivalent for Mac.
-  #   # http://lists.gnu.org/archive/html/bug-gnulib/2015-02/msg00078.html
-  #   url "https://gist.githubusercontent.com/shulima/d851f8f35526db5e2fe9/raw/f80f6a73ec102bbdea2394d9bd3482b400853f2c/gnulib-program_name.patch"
-  #   sha256 "d17d1962b98a3418a335915de8a2da219e4598d42c24555bbbc5b0c1177dd38c"
-  # end
+  patch :DATA
 
   def install
-    ENV["FUSE_CFLAGS"] = "-D_FILE_OFFSET_BITS=64 -D_DARWIN_USE_64_BIT_INODE -I/usr/local/include/osxfuse/fuse"
-    ENV["FUSE_LIBS"] = "-losxfuse -pthread -liconv"
-
+    ENV["FUSE_CFLAGS"] = "-D_FILE_OFFSET_BITS=64 -D_DARWIN_USE_64_BIT_INODE -I/usr/local/include/fuse"
+    ENV["FUSE_LIBS"] = "-lfuse -pthread -liconv"
+    ENV["LC_ALL"] = "C"
     %w[
       ncurses
       augeas
@@ -131,23 +132,25 @@ class Libguestfs < Formula
     ENV["HAVE_DPKG_FALSE"] = "#"
     ENV["HAVE_PACMAN_FALSE"] = "#"
 
+    system "git", "submodule", "update", "--init"
+    system "autoreconf", "-i"
+
     system "./configure", *args
 
-    # Build fails with just 'make install'
-    # fix for known race condition: https://bugzilla.redhat.com/show_bug.cgi?id=1614502
-    # ENV.deparallelize { system "make", "-C", "builder", "index-parse.c" }
-    # system "make", "-C", "builder", "index-scan.c"
-    # ENV.deparallelize { system "make", "-C", "builder" }
-    system "make"
-    system "make", "check" # 5 FAILs :/
-
-    system "make", "INSTALLDIRS=vendor", "DESTDIR=#{buildpath}", "install"
+    ENV.deparallelize { system "make" }
 
     libguestfs_path = "#{prefix}/var/libguestfs-appliance"
     mkdir_p libguestfs_path
     resource("fixed_appliance").stage(libguestfs_path)
+    system "make", "INSTALLDIRS=vendor", "DESTDIR=#{buildpath}", "install"
 
-    bin.install_symlink Dir["bin/*"]
+    bin.install Dir["#{buildpath}/#{prefix}/bin/*"]
+    include.install "#{buildpath}/#{prefix}/include/guestfs.h"
+    lib.install Dir["#{buildpath}/#{prefix}/lib/*"]
+    man1.install Dir["#{buildpath}/#{prefix}/share/man/man1/*"]
+    man3.install Dir["#{buildpath}/#{prefix}/share/man/man3/*"]
+    man5.install Dir["#{buildpath}/#{prefix}/share/man/man5/*"]
+
   end
 
   def caveats
@@ -172,6 +175,37 @@ class Libguestfs < Formula
 
   test do
     ENV["LIBGUESTFS_PATH"] = "#{prefix}/var/libguestfs-appliance"
-    system "#{bin}/libguestfs-test-tool", "-t 180"
+    system "make", "-j1", "quickcheck"
   end
 end
+__END__
+diff --git a/lib/Makefile.am b/lib/Makefile.am
+index 212bcb94a..84aa4297b 100644
+--- a/lib/Makefile.am
++++ b/lib/Makefile.am
+@@ -178,10 +178,11 @@ libvirt_is_version_SOURCES = libvirt-is-version.c
+ 
+ libvirt_is_version_LDADD = \
+ 	$(LIBVIRT_LIBS) \
+-	$(LTLIBINTL)
++	$(LTLIBINTL) \
++	../gnulib/lib/libgnu.la
+ 
+ libvirt_is_version_CPPFLAGS = \
+-	-DLOCALEBASEDIR=\""$(datadir)/locale"\"
++	-DLOCALEBASEDIR=\""$(datadir)/locale"\" -I$(top_srcdir)/gnulib/lib -I$(top_builddir)/gnulib/lib
+ 
+ libvirt_is_version_CFLAGS = \
+ 	$(WARN_CFLAGS) $(WERROR_CFLAGS) \
+diff --git a/lib/handle.c b/lib/handle.c
+index 290652d8c..d405768d7 100644
+--- a/lib/handle.c
++++ b/lib/handle.c
+@@ -22,6 +22,7 @@
+  */
+ 
+ #include <config.h>
++#include <errno.h>
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
